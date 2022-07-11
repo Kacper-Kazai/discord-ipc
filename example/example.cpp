59413dd2c62cffde7cef74b5512f6d6ae33b4498
Kacper Kazai <kacper.kazai@gmail.com>
2022-07-11 13:38:17 +0200

    upgrade example
    move client id and client secret from variable to program arguments
    move curl request to its own function

--- example/example.cpp
+++ example/example.cpp
@@ -2,81 +2,106 @@
 #include "discord-ipc/serialization.h"
 #include <nlohmann/json.hpp>
 
-#include <iostream>
 #include <curl/curl.h>
 
 using nlohmann::json;
 
-static std::string application_id = "*PUT CLIENT_ID HERE*";
-static std::string client_secret = "*PUT CLIENT_SECRET HERE*";
+static std::string discord_auth_token_endpoint = "https://discordapp.com/api/oauth2/token";
 
-size_t writefunc(void* ptr, size_t size, size_t nmemb, std::string* s){
-    s->append(static_cast<char*>(ptr), size * nmemb);
+size_t curl_writefunction_to_string(void* ptr, size_t size, size_t nmemb, std::string* response) {
+    response->append(static_cast<char*>(ptr), size * nmemb);
     return size * nmemb;
 }
 
-int main() {
-    ipc_connection* connection = ipc_connection::create(application_id.c_str());
+std::string get_access_token(std::string client_id, std::string client_secret, std::string code) {
+    CURL* curl;
+    CURLcode curl_code;
+    curl = curl_easy_init();
+    if (!curl) {
+        curl_easy_cleanup(curl);
+        return "";
+    }
+    curl_easy_setopt(curl, CURLOPT_URL, discord_auth_token_endpoint.c_str());
+
+    std::string response;
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_writefunction_to_string);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+
+    std::string postfields = "grant_type=authorization_code&client_id=" + client_id + "&client_secret=" + client_secret + "&code=" + code;
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postfields.c_str());
+
+    curl_code = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    if (curl_code != CURLE_OK) return "";
+
+    json response_json = json::parse(response);
+    if (!response_json.contains("access_token")) return "";
+
+    response_json.at("access_token").get_to(response);
+    return response;
+}
+
+int main(int argc, char* argv[]) {
+    std::string client_id = "";
+    std::string client_secret = "";
+
+    for (int arg_counter = 1; arg_counter < argc; arg_counter++) {
+        std::string arg = argv[arg_counter];
+        if (arg.substr(0, 2) != "--") continue;
+
+        size_t delimiter_position = arg.find("=");
+        std::string key = arg.substr(2, delimiter_position - 2);
+        std::string value = arg.substr(delimiter_position + 1, arg.length() - delimiter_position - 1);
+
+        if (key == "client_id") client_id = value;
+        else if (key == "client_secret") client_secret = value;
+    }
+    if (client_id.empty() || client_secret.empty()) {
+        printf("Please provide --client-id and --client-secret arguments.\n\nexample:\n%s --client_id=332269999912132097 --client_secret=937it3ow87i4ery69876wqire\n", argv[0]);
+        return 0;
+    }
+
+    ipc_connection* connection = ipc_connection::create(client_id.c_str());
 
     std::function<void(std::string&)> authenticate = [connection](std::string& event_data) {
         json data_json = json::parse(event_data);
 
-        packet speaking_start_packet = create_command_packet("GET_GUILDS", "");
-        connection->send(speaking_start_packet, [](std::string& event_data) {
+        packet get_guilds_packet = create_command_packet("GET_GUILDS", "");
+        connection->send(get_guilds_packet, [](std::string& event_data) {
             json data_json = json::parse(event_data);
-            std::cout << data_json.dump(2) << "\n";
+            printf("%s\n", data_json.dump(2).c_str());
         });
     };
 
-    std::function<void(std::string&)> authorization = [connection, authenticate](std::string& event_data) {
+    std::function<void(std::string&)> authorization = [client_id, client_secret, connection, authenticate](std::string& event_data) {
         json data_json = json::parse(event_data);
 
         if (!data_json.contains("code") || !data_json.at("code").is_string()) return;
         std::string code{};
         data_json.at("code").get_to(code);
 
-        CURL* curl;
-        CURLcode res;
-
-        std::string s;
-
-        curl = curl_easy_init();
-        if (curl) {
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
-            curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
-
-            std::string fields = "grant_type=authorization_code&client_id="+application_id+"&client_secret="+client_secret+"&code=" + code;
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, fields.c_str());
-
-
-            curl_easy_setopt(curl, CURLOPT_URL, "https://discordapp.com/api/oauth2/token");
-
-            res = curl_easy_perform(curl);
-
-            if (res != CURLE_OK)
-                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-            curl_easy_cleanup(curl);
+        std::string token = get_access_token(client_id, client_secret, code);
+        if (token.empty()) {
+            printf("Token exchange failed.\n");
+            return;
         }
-        json test = json::parse(s);
-        std::string token{};
-        test.at("access_token").get_to(token);
-        
+
         packet send_packet = create_authenticate_packet(token);
         connection->send(send_packet, authenticate);
     };
 
-    connection->on("READY", [connection, authorization](std::string& event_data) {
+    connection->on("READY", [client_id, connection, authorization](std::string& event_data) {
         json data_json = json::parse(event_data);
 
         std::string username{};
         data_json.at("user").at("username").get_to(username);
-        std::cout << "Welcome " << username << "!\n";
+        printf("Welcome %s!\n", username.c_str());
 
         std::vector<std::string> scopes = { "rpc" };
-        packet send_packet = create_authorize_packet(application_id.c_str(), scopes);
+        packet send_packet = create_authorize_packet(client_id.c_str(), scopes);
         connection->send(send_packet, authorization);
-    });
+        });
 
     while (true) {
         connection->loop();
